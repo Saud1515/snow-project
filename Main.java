488e9764d5d3ceaaac0306ce85d65fb335dcca7d
Saud <s.alshahrani@lancaster.ac.uk>
2026-05-04 21:30:15 +0100

    task 1

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello Snow Game!");
+
+        // board 5x4
+        char[][] board = new char[4][5];
+
+        // fill with empty spaces
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                board[i][j] = '.';
+            }
+        }
+
+        // add some pieces (example level 1)
+        board[1][1] = 'T'; // tree
+        board[2][2] = 'S'; // small snowball
+        board[3][3] = 'H'; // head
+
+        // print board
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
